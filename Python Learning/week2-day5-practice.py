ee81759cc21e1e23e1ddc19451c9bdb942b30256
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-03 12:52:27 +0500

    Week 2 Day 5 Practice Programs

--- Python Learning/week2-day5-practice.py
+++ Python Learning/week2-day5-practice.py
@@ -1,25 +1,39 @@
 #Program 01
 
-# numbers = [2, 4, 4, 6, 2, 8, 8, 10]
-# new_set = set(numbers)
-# again_list = list(new_set)
+numbers = [2, 4, 4, 6, 2, 8, 8, 10]
+new_set = set(numbers)
+again_list = list(new_set)
 
-# print(again_list)
+print(again_list)
 
 #Program 02
 
-nums = [12, 7, 15, 9, 20]
+nums = [20, 7, 15, 9, 20]
 
-max = 0
-min = 0
+max = nums[0]
+min = nums[0]
 
 for i in nums:
-    for j in nums:
-        if i > j:
-            max = i
-        elif i < j:
-            min = i
-
+    if i > max:
+        max = i
+    if i < min:
+        min = i
 
 print(f"max : {max}")
 print(f"min : {min}")
+
+#Program 03
+
+students = {
+    "Ali": 85,
+    "Sara": 78,
+    "Umer": 92,
+    "Hina": 66
+}
+
+student_name = input("Enter Student Name : ")
+
+if student_name in students:
+    print(f"{student_name} scored {students[student_name]} marks.")
+else: 
+    print("Student Not Found")
