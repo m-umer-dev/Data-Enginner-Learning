0997443c22c9f8136e03c603921a44acbee934f4
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-04 15:09:05 +0500

    Week 3 Day 2 File Handling Program practice

--- Python Learning/week3-day2-filehandling.py
+++ Python Learning/week3-day2-filehandling.py
@@ -1,19 +1,38 @@
 #Program 01
-# import os
-# file_name = input("Enter File Name : ")
+import os
+file_name = input("Enter File Name : ")
 
-# if os.path.exists(file_name):
-#     with open(file_name,'r') as f:
-#         print(f.read())
-# else :
-#     print("File does not exist")
+if os.path.exists(file_name):
+    with open(file_name,'r') as f:
+        print(f.read())
+else :
+    print("File does not exist")
 
 
 #Program 02:
 
-# import os
-# if os.path.exists("note.txt"):
-#     with open("note.txt",'r') as f:
-#         print(f.readlines().__len__())
-# else :
-#     print("File does not exist")
+import os
+if os.path.exists("note.txt"):
+    with open("note.txt",'r') as f:
+        print(f.readlines().__len__())
+else :
+    print("File does not exist")
+
+#Program 03:
+
+import os
+if os.path.exists("abc.txt"):
+    print("File already exists. Cannot overwrite.")
+else:
+    with open("abc.txt",'w') as f:
+        f.write("hello")
+
+#Program 04:
+
+import os
+if os.path.exists("lines.txt"):
+    with open("lines.txt","r") as f1:
+        with open("reversed.txt","w") as f2:
+            f2.writelines(reversed(f1.readlines()))
+else:
+    print("File does not exist")
