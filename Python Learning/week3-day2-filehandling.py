e312d3034d91d7c5f35bf3a6046077f0ec37ea43
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-04 12:17:33 +0500

    Week 3 Day 2 File handling program practice

---
+++ Python Learning/week3-day2-filehandling.py
@@ -0,0 +1,19 @@
+#Program 01
+# import os
+# file_name = input("Enter File Name : ")
+
+# if os.path.exists(file_name):
+#     with open(file_name,'r') as f:
+#         print(f.read())
+# else :
+#     print("File does not exist")
+
+
+#Program 02:
+
+# import os
+# if os.path.exists("note.txt"):
+#     with open("note.txt",'r') as f:
+#         print(f.readlines().__len__())
+# else :
+#     print("File does not exist")
