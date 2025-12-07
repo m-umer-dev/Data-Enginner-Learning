f6e3b78e983bffb88af30361f0e9f56040b6c453
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-07 23:13:14 +0500

    Week 3 Day 4 Modules and os practice

---
+++ Python Learning/week3-day4-Modules-Os.py
@@ -0,0 +1,11 @@
+#Program 01
+
+import math
+
+num = int(input("Enter Number : "))
+print(f"Square Root : {math.sqrt(num)}")
+print(f"Factorial : {math.factorial(num)}")
+
+#Program 02
+
+import random
