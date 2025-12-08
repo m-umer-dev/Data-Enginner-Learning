4be3c58309c0143cf50af645c043f2a6142ec4b6
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-08 23:20:54 +0500

    Week 3 Day 4 Module and Os automation practice

--- Python Learning/week3-day4-Modules-Os.py
+++ Python Learning/week3-day4-Modules-Os.py
@@ -8,4 +8,16 @@ print(f"Factorial : {math.factorial(num)}")
 
 #Program 02
 
-import random
+import os
+
+folder = "data_folder"
+file_name = "sample.txt"
+file_path = os.path.join(folder, file_name)
+
+try:
+    os.makedirs(folder, exist_ok=True)
+    with open(file_path, "w") as f:
+        f.write("Hello World")
+    print(f"File created successfully: {file_path}")
+except OSError as e:
+    print("Failed to create file:", e)
