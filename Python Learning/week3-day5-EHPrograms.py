ba0b2c71f19ff345c0d01391181391e40952160e
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-09 23:33:42 +0500

    Week 3 Day 5 Intermediate Exception Handling Program practice

---
+++ Python Learning/week3-day5-EHPrograms.py
@@ -0,0 +1,40 @@
+#Program 01
+
+# try:
+#     num1 = int(input("Enter 1st Number : "))
+#     num2 = int(input("Enter 2nd Number : "))
+#     num3 = int(input("Enter 3rd Number : "))
+
+#     print(f"Divide {num1/num2/num3}")
+
+# except ZeroDivisionError:
+#     print("Number Should not be divide by zero")
+
+# except ValueError:
+#     print("All Number Must be integers")
+
+#Program 02
+
+# try:
+#     f = open ('lines.txt','r')
+#     file_data = f.read()
+# except FileNotFoundError:
+#     print("File Not Found")
+# else:
+#     print(f"Data Read Successfully : {file_data}")
+# finally:
+#     print("This will run everytime")
+
+#Program 03
+
+# try:
+#     age = int(input("Enter Age : "))
+#     if age < 0:
+#         raise ValueError("Not Eligible")
+#     else:
+#         print("Eligible")
+# except ValueError as e:
+#     print(e)
+
+
+#Program 04
