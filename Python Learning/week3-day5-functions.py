8872be01261e937527f71a23fbd762e634bbe6a1
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-12 23:30:18 +0500

    Week 3 Day 5 Function concept practice

--- Python Learning/week3-day5-functions.py
+++ Python Learning/week3-day5-functions.py
@@ -1,12 +1,24 @@
 #Program 01
 
-def count_even (numbers):
-    for i in numbers:
+def count_even (n):
+    count = 0
+    for i in n:
         if i % 2 == 0:
-            count += 1
+            count +=1
 
     return count
 
-num_list = list(input("Enter Numbers : "))
+num_list = [1,2,3,4,5]
 final_count = count_even(num_list)
 print(final_count)
+
+#Program 02
+
+def bur_fun (items):
+    print(items)
+
+products = {
+    "mouse": 500,
+    "keyboard": 1500,
+    "usb": 300
+}
