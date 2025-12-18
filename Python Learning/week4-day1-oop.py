76edc7afb1a9ba12527af328d18aee136d46dfe5
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-18 23:15:52 +0500

    Oops concept practice

--- Python Learning/week4-day1-oop.py
+++ Python Learning/week4-day1-oop.py
@@ -7,3 +7,12 @@ class Student:
 
     def show_details(self):
         print(f"Name: {self.name}, Marks: {self.marks}")
+
+
+p1 = Product("Mouse", 500)
+p2 = Product("Keyboard", 1500)
+
+p1.show_product()
+p2.show_product()
+
+print(p1.get_price())
