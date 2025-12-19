c59f5170176ed1ebf97a019f84d191e37cc7456a
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2025-12-19 22:53:02 +0500

    Week 4 day 01 oops practice program

--- Python Learning/week4-day1-oop.py
+++ Python Learning/week4-day1-oop.py
@@ -1,18 +1,14 @@
 #Program 01
 
-class Student:
-    def __init__(self, name, marks):
+class Employee:
+    def __init__(self,name,salary):
         self.name = name
-        self.marks = marks
+        self.salary = salary
 
-    def show_details(self):
-        print(f"Name: {self.name}, Marks: {self.marks}")
-
-
-p1 = Product("Mouse", 500)
-p2 = Product("Keyboard", 1500)
-
-p1.show_product()
-p2.show_product()
-
-print(p1.get_price())
+    def show_info(self):
+        return f"Name : {self.name} , Salary : {self.salary}"
+    
+d1 = Employee("Data Engineer",30000)
+d2 = Employee("Python",10000)
+print(d1.show_info())
+print(d2.show_info())
