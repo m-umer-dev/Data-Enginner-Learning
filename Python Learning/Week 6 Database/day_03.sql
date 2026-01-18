19eb69c10f620a489f8c52b73bc4e3ee1db4db17
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-01-18 16:18:14 +0500

    Day 03 SQL Subqueries & Query Optimization

---
+++ Python Learning/Week 6 Database/day_03.sql
@@ -0,0 +1,33 @@
+SELECT AVG(salary) FROM employee;
+
+SELECT name, salary
+FROM employee
+WHERE salary > (
+    SELECT AVG(salary) FROM employee
+);
+
+SELECT name
+FROM employee
+WHERE dept_id IN (
+    SELECT dept_id
+    FROM department
+    WHERE dept_name = 'IT'
+);
+
+SELECT name
+FROM employee e
+WHERE EXISTS (
+    SELECT 1
+    FROM department d
+    WHERE d.dept_id = e.dept_id
+);
+
+SELECT name, salary
+FROM employee e
+WHERE salary > (
+    SELECT AVG(salary)
+    FROM employee
+    WHERE dept_id = e.dept_id
+);
+
+CREATE INDEX idx_dept on employee(dept_id);
