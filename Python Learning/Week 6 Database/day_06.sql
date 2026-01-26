5d97847eab1c485c93365d0dc03275f39e42a4aa
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-01-26 23:02:43 +0500

    Advanced Joins & Subqueries

--- Python Learning/Week 6 Database/day_06.sql
+++ Python Learning/Week 6 Database/day_06.sql
@@ -1,6 +1,24 @@
-SELECT department, AVG(salary)
-FROM employee
-GROUP BY department > (
-    SELECT department, AVG(salary)
-    FROM employee
+-- Task 01
+
+SELECT e.name, e.salary
+FROM employee e
+WHERE EXISTS (
+    SELECT 1
+    FROM employee x
+    WHERE e.department = x.department
+    AND e.salary < x.salary
 )
+
+-- Task 02
+
+WITH depts_avg AS (
+    SELECT department, AVG(salary) AS dept_avg
+    FROM employee
+    GROUP BY department
+)
+SELECT department
+FROM depts_avg
+WHERE dept_avg > (
+    SELECT AVG(dept_avg)
+    FROM depts_avg
+);
