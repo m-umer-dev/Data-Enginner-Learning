e9c6eedd7ca762724ff3bfc0fb9524ca37f66688
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-01-28 23:00:40 +0500

    Week 7 Day 01 ETL Pipeline Builing

---
+++ Python Learning/Week 7 ETL piplines/etl_project/etl.py
@@ -0,0 +1,27 @@
+import csv
+
+def extract_sales(path):
+    with open(path, newline="") as f:
+        return list(csv.DictReader(f))
+
+def transform_sales(data):
+    cleaned = []
+    for row in data:
+        amount = int(row["amount"])
+        if amount >= 100:
+            row["amount"] = int(amount * 1.10)
+            cleaned.append(row)
+    return cleaned
+
+def load_sales(data, path):
+    with open(path, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["name", "amount"])
+        writer.writeheader()
+        writer.writerows(data)
+
+if __name__ == "__main__":
+    raw_data = extract_sales("data/raw/sales.csv")
+    final_data = transform_sales(raw_data)
+    load_sales(final_data, "data/processed/sales_clean.csv")
+
+    print("ETL Pipeline Completed Successfully")
