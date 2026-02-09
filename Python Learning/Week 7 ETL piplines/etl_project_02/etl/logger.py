4c6115976e8daf2888d596a2e04668a78ee24a89
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-02-09 16:14:20 +0500

    ETL Building Project 02 Configuration & Environment Management

--- Python Learning/Week 7 ETL piplines/etl_project_02/etl/logger.py
+++ Python Learning/Week 7 ETL piplines/etl_project_02/etl/logger.py
@@ -1,7 +1,10 @@
 import logging
+from config import ENV
+
+level = logging.DEBUG if ENV == "dev" else logging.INFO
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=level,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
