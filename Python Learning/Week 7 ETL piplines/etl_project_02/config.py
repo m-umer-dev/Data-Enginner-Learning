9d43f0962b338711c2bb327637190b82362c2088
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-02-09 16:13:57 +0500

    ETL Building Project 02 Configuration & Environment Management

--- Python Learning/Week 7 ETL piplines/etl_project_02/config.py
+++ Python Learning/Week 7 ETL piplines/etl_project_02/config.py
@@ -1,7 +1,12 @@
-# File paths
-RAW_SALES_PATH = "data/raw_sales.csv"
-PROCESSED_SALES_PATH = "data/processed_sales.csv"
+import os
+from dotenv import load_dotenv
 
-# Business rules
-MIN_SALE_AMOUNT = 200
-HIGH_SALE_LIMIT = 500
+load_dotenv()
+
+RAW_SALES_PATH = os.getenv("RAW_SALES_PATH")
+PROCESSED_SALES_PATH = os.getenv("PROCESSED_SALES_PATH")
+
+MIN_SALE_AMOUNT = int(os.getenv("MIN_SALE_AMOUNT"))
+HIGH_SALE_LIMIT = int(os.getenv("HIGH_SALE_LIMIT"))
+
+ENV = os.getenv("ENV", "dev")
