cdfec82cd2329fb31f5c87297daedffde2f7841d
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-02-18 23:14:11 +0500

    Project 04 Multiple file merge plus data cleaned

--- Python Learning/Week 8 Practice Projects/project_4_mlti_Fle_Mrge_bsinss_lgic/main.py
+++ Python Learning/Week 8 Practice Projects/project_4_mlti_Fle_Mrge_bsinss_lgic/main.py
@@ -17,7 +17,70 @@ def extract_order(path):
     except FileNotFoundError:
         return []
     
+def transform(cust_data, order_data):
+    cleaned_cust = []
+    cleaned_order = []
+    final_data = []
+
+    for row in cust_data:
+        try:
+            customer_id = int(row["customer_id"].strip())
+            name = row["name"].strip()
+            city = row["city"].strip()
+        except:
+            continue
+
+        if not city:
+            continue
+
+        cleaned_cust.append({
+            "customer_id": customer_id,
+            "name": name,
+            "city": city
+        })
+
+    # Create lookup dictionary (Very Important Technique 🔥)
+    cust_lookup = {c["customer_id"]: c for c in cleaned_cust}
+
+    for row in order_data:
+        try:
+            order_id = int(row["order_id"].strip())
+            customer_id = int(row["customer_id"].strip())
+            amount = int(row["amount"].strip())
+        except:
+            continue
+
+        if amount < 0:
+            continue
+
+        if customer_id not in cust_lookup:
+            continue
+
+        customer = cust_lookup[customer_id]
+
+        final_data.append({
+            "order_id": order_id,
+            "customer_name": customer["name"],
+            "city": customer["city"],
+            "amount": amount
+        })
+
+    return final_data
+
+def load(path,data): 
+    try: 
+        with open(path,'w',newline='') as f: 
+            writer = csv.DictWriter(
+                f,
+                fieldnames=["order_id","customer_name","city","amount"]) 
+            writer.writeheader() 
+            writer.writerows(data) 
+    except FileNotFoundError: 
+        return []
 
 
-print(extract_cust('customers.csv'))
-print(extract_order('orders.csv'))
+
+cust_data = extract_cust('customers.csv')
+order_data = extract_order('orders.csv')
+business = transform(cust_data,order_data)
+load('final_data.csv',business)
