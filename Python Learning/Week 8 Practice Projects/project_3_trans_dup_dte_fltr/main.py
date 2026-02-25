580d1e6c93b46a174759ecde96cf46904c117a6a
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-02-25 20:20:07 +0500

    Project 3 transection duplicate + date_filter

--- Python Learning/Week 8 Practice Projects/project_3_trans_dup_dte_fltr/main.py
+++ Python Learning/Week 8 Practice Projects/project_3_trans_dup_dte_fltr/main.py
@@ -1,16 +1,28 @@
 import csv
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=("%(asctime)s - %(levelname)s - %(message)s")
+)
+
+logger = logging.getLogger(__name__)
 
 def extract(path):
     try:
         with open(path,newline='') as f:
-            return list(csv.DictReader(f))
+            data = list(csv.DictReader(f))
+            logger.info(f"{len(data)} Row is loaded")
+            return data
     except FileNotFoundError:
+        logger.info("File is not found")
         return []
     
 
 def transform(data):
     dup_clean = set()
     cleaned = []
+    logger.info("Tranform is started")
     for row in data:
         try:
             transaction_id = int(row["transaction_id"].strip())
@@ -19,6 +31,7 @@ def transform(data):
             date = row["date"].strip()
             year = int(row["date"].split("-")[0].strip())
         except (KeyError,ValueError):
+            logger.info("Invalid Rows skipped")
             continue
         if amount < 0:
             continue
@@ -40,6 +53,7 @@ def transform(data):
             "date" : date,
             "size" : size
         })
+    logger.info("Data is Cleaned")
 
     return cleaned
 
@@ -51,7 +65,9 @@ def load(path,data):
             writer = csv.DictWriter(f,fieldnames=["transaction_id","customer","amount","date","size"])
             writer.writeheader()
             writer.writerows(data)
+            logger.info("Rows is loaded")
     except FileNotFoundError:
+        logger.info("File not found")
         return []
     
 
