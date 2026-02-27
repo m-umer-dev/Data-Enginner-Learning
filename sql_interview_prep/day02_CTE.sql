de54f60764153b6ce43c9066e69a2d4edda252d5
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-02-27 16:59:46 +0500

    Common Table Expression Practice

---
+++ sql_interview_prep/day02_CTE.sql
@@ -0,0 +1,48 @@
+-- 👉 Find employees who earn more than the average salary of their department.
+WITH total AS (
+    SELECT
+        name,
+        department,
+        salary,
+        AVG(salary) OVER(
+            PARTITION BY department
+        ) AS dept_avg
+    FROM employees
+)
+
+SELECT *
+FROM total
+WHERE salary > dept_avg
+
+-- 👉 Find employees whose salary is the 2nd highest in their department.
+WITH emp_id AS (
+    SELECT
+        name,
+        department,
+        salary,
+        DENSE_RANK() OVER(
+            PARTITION BY department ORDER BY salary DESC
+        ) AS salary_rank
+    FROM employees
+)
+
+SELECT * 
+FROM emp_id
+WHERE salary_rank = 2;
+
+-- 👉 Find employees whose salary is higher than the previous employee in the same department (based on salary order).
+WITH emp_pre_sal AS (
+    SELECT
+        name,
+        department,
+        salary,
+        LAG(salary) OVER(
+            PARTITION BY department 
+            ORDER BY salary
+        ) AS previous_salary
+    FROM employees
+)
+
+SELECT * 
+FROM emp_pre_sal
+WHERE salary > previous_salary
