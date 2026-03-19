52f4dce2915af584c3171969af16609f433a8199
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-03-19 22:56:19 +0500

    Excerise 01

--- sql_interview_prep/exercise01.sql
+++ sql_interview_prep/exercise01.sql
@@ -100,3 +100,25 @@ JOIN customer c
 JOIN products p 
     ON o.product_id = p.product_id
 WHERE c.country = 'Pakistan';
+
+SELECT 
+    c.country,
+    SUM(p.price * o.quantity) AS total_revenue
+FROM orders o
+JOIN customer c 
+    ON o.customer_id = c.customer_id
+JOIN products p 
+    ON o.product_id = p.product_id
+GROUP BY c.country 
+ORDER BY total_revenue DESC;
+
+SELECT 
+    c.name,
+    SUM(p.price * o.quantity) AS total_spent,
+    RANK() OVER (ORDER BY SUM(p.price * o.quantity) DESC) AS rank
+FROM orders o
+JOIN customer c 
+    ON o.customer_id = c.customer_id
+JOIN products p 
+    ON o.product_id = p.product_id
+GROUP BY c.name DESC;
