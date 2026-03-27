4e49c0c60c98c65a672bc2e88eb03b555c5661e0
Muhammad Umer Amir <161680343+m-umer-dev@users.noreply.github.com>
2026-03-27 23:53:26 +0500

    Excerise 02

--- sql_interview_prep/exercise02.sql
+++ sql_interview_prep/exercise02.sql
@@ -28,8 +28,41 @@ WHERE rnk = 1;
 
 SELECT
     c.name,
-    -- COUNT(o.order_id) AS cnt_orders,
-    AVG(cnt_orders) AS avg_orders
+    AVG(o.order_id) AS avg_orders
 FROM orders o
 JOIN customer c ON o.customer_id = c.customer_id
+GROUP BY c.name
+
+
+SELECT
+    c.name,
+    SUM(p.price * o.quantity) / COUNT(o.order_id) AS total_value
+FROM orders o
+JOIN customer c ON o.customer_id = c.customer_id
+JOIN products p ON o.product_id = p.product_id
+GROUP BY c.name
+
+
+SELECT * 
+FROM
+    (SELECT
+        c.name,
+        SUM(p.price * o.quantity) AS total_spent,
+        COUNT(o.order_id) AS total_orders
+    FROM orders o
+    JOIN customer c ON o.customer_id = c.customer_id
+    JOIN products p ON o.product_id = p.product_id
+    GROUP BY c.name) t
+
+WHERE total_spent > 1000 AND total_orders > 1
+
+
+
+SELECT
+    c.name,
+    SUM(p.price * o.quantity) AS customer_spent,
+    (customer_spent / total_revenue) * 100 AS final_out
+FROM orders o
+JOIN customer c ON o.customer_id = c.customer_id
+JOIN products p ON o.product_id = p.product_id
 GROUP BY c.name
